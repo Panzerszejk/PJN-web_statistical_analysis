0b38d6e409ddfe31b064179cd7d836ec4ab4d93a
Pawel Armatys <piwirm@gmail.com>
2018-10-29 09:50:53 +0100

    File exist check
    Sprawdzenie czy plik do pobrania już istnieje

--- data_extractor.py
+++ data_extractor.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import ssl
 from urllib.request import Request, urlopen
-
+from pathlib import Path
 
 def get_post_content(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -28,20 +28,26 @@ def get_post_content(url):
 
 
 def get_posts(start, end):
+    posts = []
     for pageNumber in range(start, end+1):
         req = Request('https://zaufanatrzeciastrona.pl/page/' + str(pageNumber), headers={'User-Agent': 'Mozilla/5.0'})
         context = ssl._create_unverified_context()
         webpage = urlopen(req, context=context).read()
         soup = BeautifulSoup(webpage, 'html.parser')
-        posts = []
         for post in soup.find_all('div'):
             if 'class' in post.attrs:
                 if 'thumbnail-wrap' in post['class']:
                     posts.append(post.a['href'])
-        return posts
+        print(pageNumber)
+    return posts
 
 
 #print(get_post_content("https://zaufanatrzeciastrona.pl/post/przed-nami-advanced-threat-summit-2018-a-dla-was-kod-rabatowy/"))
-for post in get_posts(1,1):
-    with open('posts/'+ post.split('/')[4] + '.txt', 'w') as f:
-        f.write(get_post_content(post))
+for post in get_posts(1, 20):
+    print(post)
+    if not Path('posts/' + post.split('/')[4] + '.txt').exists():
+        print("file don't exist")
+        with open('posts/' + post.split('/')[4] + '.txt', 'w') as f:
+            f.write(get_post_content(post))
+    else:
+        print("file exist")
