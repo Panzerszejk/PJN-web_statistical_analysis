35beed3d344b6548a8e8c00ccb0dd68903f57760
Konrad Bober <konradbober1995@gmail.com>
2018-11-03 15:22:27 +0100

    Dodana funkcja do pobierania podstrony z recenzjami oraz małe uogólnienie funkcji do listy artykółów

--- data_extractor.py
+++ data_extractor.py
@@ -20,25 +20,61 @@ def get_post_content(url):
     #        text += "\n" + line
     return text
 
+def get_post_content2(url):
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    context = ssl._create_unverified_context()
+    webpage = urlopen(req, context=context).read()
+    soup = BeautifulSoup(webpage, 'html.parser')
+    text = ""
 
-def get_posts(start, end):
+    while True:
+        for article in soup.find_all('article', {'class': 'word-txt'}):
+            for paragraph in article.find_all("p",recursive=False):
+                text += "\n" + paragraph.getText()
+
+        btn = soup.find('div', {'class': 'np-right'})
+        if btn:
+            nextpage=btn.a['href']
+            nexturl= url.split('?') # strony wystepuja 2 odmianach więc na sztywno nie przypisze a link zawiera tylko końcowkę zaczynając od '?'
+            req = Request(nexturl[0]+nextpage, headers={'User-Agent': 'Mozilla/5.0'})
+            print('     '+nexturl[0]+nextpage)
+            context = ssl._create_unverified_context()
+            webpage = urlopen(req, context=context).read()
+            soup = BeautifulSoup(webpage, 'html.parser')
+        else:
+            break
+
+    return text
+
+
+def get_posts(start, end, page, divname):
     posts = []
     for pageNumber in range(start, end+1):
         print(pageNumber)
-        req = Request('https://www.gry-online.pl/gry/22-' + str(pageNumber), headers={'User-Agent': 'Mozilla/5.0'})
+        req = Request(page + str(pageNumber), headers={'User-Agent': 'Mozilla/5.0'})
         context = ssl._create_unverified_context()
         webpage = urlopen(req, context=context).read()
         soup = BeautifulSoup(webpage, 'html.parser')
-        for div in soup.find_all("div", {'class': 'lista-gry'}):
+        for div in soup.find_all("div", {'class': divname}):
             for box in div.find_all("div", {'class': 'box'}):
                 posts.append('https://www.gry-online.pl' + box.a['href'])
     return posts
 
-
 #print(get_posts(1, 1))
 #print(get_post_content("https://www.gry-online.pl/gry/soulcalibur-vi/zc50dc"))
-for post in get_posts(1, 854):
+# for post in get_posts(1, 854, 'https://www.gry-online.pl/gry/22-','lista-gry'):
+#     print(post)
+#     if not Path('posts/' + post.split('/')[4] + '.txt').exists():
+#         with open('posts/' + post.split('/')[4] + '.txt', 'w') as f:
+#             f.write(get_post_content(post))
+
+for post in get_posts(1, 10, 'https://www.gry-online.pl/recenzje-gier.asp?STR=','lista'): #121 ostatnia strona
     print(post)
-    if not Path('posts/' + post.split('/')[4] + '.txt').exists():
-        with open('posts/' + post.split('/')[4] + '.txt', 'w') as f:
-            f.write(get_post_content(post))
+    if not Path('posts/' + post.split('/')[3] + '.txt').exists():
+        with open('posts/' + post.split('/')[3] + '.txt', 'w') as f:
+            f.write(get_post_content2(post))
+
+
+#print(get_post_content2('https://www.gry-online.pl/S020.asp?ID=13040 '))
+#print(get_post_content2('https://www.gry-online.pl/S020.asp?ID=1366')) #bez stron
+#print(get_post_content('https://www.gry-online.pl/gry/red-dead-redemption-ii/zb4a0f'))
