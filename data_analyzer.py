fdd6d6e5c6cfed3bef1d9125365d1f84231a1373
Pawel Armatys <piwirm@gmail.com>
2018-11-07 11:52:02 +0100

    Merged in one plot

--- data_analyzer.py
+++ data_analyzer.py
@@ -19,32 +19,27 @@ all_text = all_text.replace('\'', '')
 all_text = all_text.lower()
 word_list = all_text.split()
 
-<<<<<<< HEAD
 print(len(word_list))
-=======
 print("File read complete")
->>>>>>> 21493157b0d734d2e06ce9132d35f1681aa36257
 
 labels = []
 sizes = []
-suma = 0
+sum20 = 0
+sumrest = 0
 for i, element in enumerate(Counter(word_list).most_common()):
-    if i < 19:
+    if i < 20:
         labels.append(element[0])
         sizes.append(element[1])
-<<<<<<< HEAD
+        sum20 += element[1]
     else:
-        suma += element[1]
-labels.append('INNE')
-sizes.append(suma)
-=======
+        sumrest += element[1]
 
 print("Most common complete")
 
 words_dict = Counter()
 word_count = 0
 word_unique_count = 0
-plot_count_list =[0]
+plot_count_list = [0]
 
 for word in word_list:
     word_count += 1
@@ -62,11 +57,22 @@ print("Words total: "+str(word_count)+' unique: '+str(word_unique_count))
 #colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
 
 # Plot
->>>>>>> 21493157b0d734d2e06ce9132d35f1681aa36257
-plt.pie(sizes, labels=labels, autopct='%1.1f%%')
-plt.axis('equal')
-plt.show()
+#plt.pie(sizes, labels=labels, autopct='%1.1f%%')
+#plt.axis('equal')
+#plt.show()
 
 plot_count_labels = np.arange(0,word_count,50000)
-plt.plot(plot_count_labels,plot_count_list, 'ro')
+#plt.plot(plot_count_labels,plot_count_list, 'ro')
+#plt.show()
+
+
+fig, axes = plt.subplots(nrows=2, ncols=2)
+axes[0, 0].pie([sum20, sumrest], labels=['20 most popular words', 'The rest of the words'], autopct='%1.1f%%', startangle=60)
+axes[0, 0].set_title('Words distribution')
+axes[0, 1].pie(sizes, labels=labels, autopct='%1.1f%%')
+axes[0, 1].set_title('20 most popular words distribution')
+axes[1, 0].plot(plot_count_labels,plot_count_list, 'ro')
+axes[1, 0].set_title('Number of unique words growth')
+
+fig.subplots_adjust(hspace=0.3)
 plt.show()
