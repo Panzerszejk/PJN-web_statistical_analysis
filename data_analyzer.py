cc9f5aeb9d8f2691565df22508a34fe24281997d
Pawel Armatys <piwirm@gmail.com>
2018-11-07 12:45:04 +0100

    Added text coverage plot

--- data_analyzer.py
+++ data_analyzer.py
@@ -26,7 +26,12 @@ labels = []
 sizes = []
 sum20 = 0
 sumrest = 0
+tclabels = []
+tcsizes = []
 for i, element in enumerate(Counter(word_list).most_common()):
+    if i < 15:
+        tclabels.append(element[0])
+        tcsizes.append(element[1])
     if i < 20:
         labels.append(element[0])
         sizes.append(element[1])
@@ -69,10 +74,14 @@ plot_count_labels = np.arange(0,word_count,50000)
 fig, axes = plt.subplots(nrows=2, ncols=2)
 axes[0, 0].pie([sum20, sumrest], labels=['20 most popular words', 'The rest of the words'], autopct='%1.1f%%', startangle=60)
 axes[0, 0].set_title('Words distribution')
+axes[0, 0].axis('equal')
 axes[0, 1].pie(sizes, labels=labels, autopct='%1.1f%%')
 axes[0, 1].set_title('20 most popular words distribution')
+axes[0, 1].axis('equal')
 axes[1, 0].plot(plot_count_labels,plot_count_list, 'ro')
 axes[1, 0].set_title('Number of unique words growth')
+axes[1, 1].bar(tclabels, tcsizes)
+axes[1, 1].set_title('Text coverage')
 
 fig.subplots_adjust(hspace=0.3)
 plt.show()
