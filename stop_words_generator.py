cd85cdb1853701f6be0fc72754dbeb54c5fba79b
Konrad Bober <konradbober1995@gmail.com>
2018-11-19 20:16:58 +0100

    New method avg, std_dev and normalization

--- stop_words_generator.py
+++ stop_words_generator.py
@@ -1,6 +1,7 @@
 import os
 from collections import Counter
 import pickle
+import math
 
 def most_common():
     all_text = ""
@@ -18,7 +19,7 @@ def most_common():
     word_list = all_text.split()
 
     most_common = Counter(word_list).most_common()
-    list_common=[]
+    list_common = []
     for w in most_common:
         list_common.append(w[0])
 
@@ -28,7 +29,8 @@ def most_common():
 def new_method(num):
     all_text = ""
     N = 0
-    avg_x = dict()
+    stop_words = dict()
+    measures = dict()
     for file in os.listdir("posts"):
         if file.endswith(".txt"):
             with open('posts/' + file, 'r') as f:
@@ -48,7 +50,37 @@ def new_method(num):
     for word in word_list:
         words_dict[word] += 1
 
+    avg = 0
+    std_dev = 0
     for key,value in words_dict.items():
-        avg_x[key] = value/N
+        avg = value/N
+        q = (value - 1) / N
+        xqN = value - q * N
+        std_dev = math.sqrt((xqN*(((q+1)-avg)**2) + (N - xqN)*((q-avg)**2))/(N-1))
+        measures[key] = (avg, std_dev)
+
+    min_avg = avg
+    max_avg = min_avg
+    min_dev = std_dev
+    max_dev = min_dev
+
+    for key, value in measures.items():
+        if value[0] < min_avg:
+            min_avg = value[0]
+        else:
+            if value[0] > max_avg:
+                max_avg=value[0]
+
+        if value[1] < min_dev:
+            min_dev = value[1]
+        else:
+            if value[1] > max_dev:
+                max_dev=value[1]
+
+
+    for key, value in measures.items():
+        stop_words[key]=(value[0]-min_avg)/max_avg + 1-(value[1]-min_dev)/max_dev
+
+
 
 most_common()
